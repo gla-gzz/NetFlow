f64abcc4adb9eb11172dd04eba5625d12150a352
A. Gzz <35881476+gla-gzz@users.noreply.github.com>
2018-02-27 21:49:49 -0600

    Added second version.

--- Homework2/NF_2.py
+++ Homework2/NF_2.py
@@ -1,13 +1,16 @@
 #Homework No.2
 from grafo import Grafo
+from random import random
+"""G = Grafo()
+G.add_some('a', 0.5, 0.8, 0.4, 0.01)
+G.add_some('b', 0.5, 0.5, 0.7, 0.20)
+G.add_some('c', 0.9, 0.1, 0.3, 0.11)
+G.connect_these('a', 'b')
+C = G.complement_me()"""
+
 G = Grafo()
-G.add_some('a')
-G.add_some('b')
-G.add_more('a', 0.5, 0.8, 0.4, 0.01)
-G.add_more('b', 0.3, 0.5, 0.7, 0.20)
-print("Coordenadas x: ", G.X)
-print("Coordenadas y: ", G.Y)
-print("Tamano: ", G.size)
-print("Color: ", G.color)
-print(G.V)
+n = 10
+for i in range(n):
+    G.add_some("nodo" + str(i+1), i+1, i+1, random(), random())
+G.saveme_to("nodos.dat")
 
--- Homework2/grafo.py
+++ Homework2/grafo.py
@@ -1,19 +1,37 @@
+#Homework No.2
 class Grafo:
-    def __init__(self):                          
-        self.V = set()
+    def __init__(self):                             #Initialices graph             
+        self.N = set()
         self.X = dict()
         self.Y = dict()
         self.size = dict()
         self.color = dict()
-        self.vecinos = dict()
-    def add_some(self, v):                      
-        self.V.add(v)
-        if not v in self.vecinos:
-            self.vecinos[v] = set()
-    def add_more(self, V, x, y, w, z):                
-        self.X[V] = x
-        self.Y[V] = y
-        self.size[V] = w
-        self.color[V] = z
-        
+        self.vecinos = dict()  
+        self.E = dict()
+    def add_some(self, n, x=0, y=0, w=0, z=0):      #Adds a node and it's info to the graph
+        self.N.add(n)              
+        self.X[n] = x
+        self.Y[n] = y
+        self.size[n] = w
+        self.color[n] = z
+        if not n in self.vecinos:
+            self.vecinos[n] = set()
+    def connect_these(self, a, b, peso=1):          #Conects given nodes
+        self.E[(a, b)] = self.E[(b, a)] = peso
+        self.vecinos[a].add(b)
+        self.vecinos[b].add(a)
+    def complement_me(self):                        #Complement of the links in the graph
+        comp = Grafo()
+        for n in self.N:
+            for m in self.N:
+                if n != m and (n, m) not in self.E:
+                    comp.add_some(n, self.X[n], self.Y[n], self.size[n], self.color[n])
+                    comp.add_some(m, self.X[m], self.Y[m], self.size[m], self.color[m])
+                    comp.connect_these(n, m)
+        return comp
+    def saveme_to(self, saving="data.dat"):         #Saves the data of the graph to a file
+        with open(saving, 'w') as archivo:
+            for n in self.N:
+                print(n, self.X[n], self.Y[n], self.size[n], self.color[n], sep = ' ', file=archivo)
 
+    
